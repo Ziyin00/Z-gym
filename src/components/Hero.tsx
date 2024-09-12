5c807418f6efb62504c966daf018aa6d250bb884
Yusuf <usufee42@Gmail.com>
2024-09-12 21:37:30 +0300

    hero responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import HeroSlider from "./HeroSlider";
 const Hero = () => {
   return (
     <section
-      className="h-[80vh] md:h-[150vh] lg:h-[912px] bg-hero bg-cover bg-center bg-no-repeat"
+      className="h-[150vh] md:h-[150vh] lg:h-[912px] bg-hero bg-cover bg-center bg-no-repeat"
       id="home"
     >
       <div className="container mx-auto h-full">
